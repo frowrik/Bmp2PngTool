bad93c9a90388885fa38c2771f0b917e37e66642
Vladimir Shatrov <frowrik@yandex.ru>
2021-09-07 21:13:19 +0500

    fixs

--- sources/BMPTOPNGRECCONVERTER.cpp
+++ sources/BMPTOPNGRECCONVERTER.cpp
@@ -14,9 +14,13 @@ void processImageToPNG(const char* nameIn, const char* nameOut) {
     int channels_in_file;
     stbi_uc* imagein = stbi_load(nameIn, &w, &h, &channels_in_file, 0);
     if (imagein) {
-        stbi_write_png(nameOut, w, h, channels_in_file, imagein, w * channels_in_file);
+        if (stbi_write_png(nameOut, w, h, channels_in_file, imagein, w * channels_in_file) == 0) {
+            std::cout << "not save image " << nameOut << "\n";
+        }
 
         stbi_image_free(imagein);
+    } else {
+        std::cout << "not open image " << nameIn << "\n";
     }
 }
 
@@ -38,7 +42,9 @@ void recursive_processBMP(const std::filesystem::path& src, const std::filesyste
                     
                     if (!std::filesystem::exists(fileoutpatch)){
                         processImageToPNG(fileinpatch.c_str(), fileoutpatch.c_str());
-                        std::cout << fileoutpatch << "\n";
+                        std::cout << fileinpatch << " to " << fileoutpatch << "\n";
+                    } else {
+                        std::cout << "not convert, image already exists " << fileoutpatch << "\n";
                     }
                 }
             } else {
@@ -49,51 +55,38 @@ void recursive_processBMP(const std::filesystem::path& src, const std::filesyste
     }
 }
 
-void recursive_processALL(const std::filesystem::path& src, const std::filesystem::path& dst) {
-    if (std::filesystem::is_directory(src)) {
-        std::filesystem::create_directories(dst);
-
-        auto diriter = std::filesystem::directory_iterator(src);
-
-        for (auto const& item : diriter) {
-            if (item.is_regular_file()) {
-                auto filepath = item.path();
-                auto name = filepath.filename();
-                auto ext = filepath.extension();
-
-                {
-                    auto fileinpatch = (src / item.path()).generic_u8string();
-                    auto fileoutpatch = (dst / item.path().stem()).generic_u8string() + ".png";
-
-                    if (!std::filesystem::exists(fileoutpatch)) {
-                        processImageToPNG(fileinpatch.c_str(), fileoutpatch.c_str());
-                        std::cout << fileoutpatch << "\n";
-                    }
-                }
-            }
-            else {
-                recursive_processALL(item.path(), dst / item.path().filename());
-            }
-        }
-
-    }
-}
-
 int main(int argc, char** argv) {
-    
+    std::cout << "example cmd 1: bmptopngconv input_dir output_dir\n";
+    std::cout << "example cmd 2: bmptopngconv input_bmp output_png\n";
     if (argc > 2) {
-        std::cout << "dir bmp input";
+        std::cout << "bmp or dir input ";
         std::cout << argv[1];
         std::cout << "\n";
 
-        std::cout << "dir png output";
+        std::cout << "png or dir output ";
         std::cout << argv[2];
         std::cout << "\n";
 
-        std::filesystem::path In = std::filesystem::u8path(argv[1]);
-        std::filesystem::path Out = std::filesystem::u8path(argv[2]);
+        std::filesystem::path In    = std::filesystem::absolute(std::filesystem::u8path(argv[1]));
+        std::filesystem::path Out   = std::filesystem::absolute(std::filesystem::u8path(argv[2]));
 
-        recursive_processBMP(In, Out);
+        std::filesystem::create_directories(Out.parent_path());
+
+        if (std::filesystem::is_regular_file(In)) {
+            if (In.extension().compare(L".bmp") == 0) {
+                auto fileinpatch    = In.generic_u8string();
+                auto fileoutpatch   = Out.generic_u8string();
+
+                if (!std::filesystem::exists(fileoutpatch)) {
+                    processImageToPNG(fileinpatch.c_str(), fileoutpatch.c_str());
+                    std::cout << fileinpatch << " to " << fileoutpatch << "\n";
+                } else {
+                    std::cout << "not convert, image already exists " << fileoutpatch << "\n";
+                }
+            }
+        } else {
+            recursive_processBMP(In, Out);
+        }
     }
 
     return 0;
